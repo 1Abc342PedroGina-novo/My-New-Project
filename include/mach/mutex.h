4a6ff20ec804c9710069528f97829d623128fe8c
1Abc342PedroGina-novo <rmagalhaesjuridico5@gmail.com>
2026-08-08 09:38:51 -0300

    Add mutex structure definition in mutex.h

---
+++ include/mach/mutex.h
@@ -0,0 +1,26 @@
+/* SPDX License Indentifier: GPL-2.0 */
+/* created by Pedro Emanuel */
+/* HISTORIC:
+ * 9:37 08/08/26 
+ * i make struct mutex 
+ * and
+ */
+
+#ifndef MACH_MUTEX_H
+#define MACH_MUTEX_H
+
+#include <asm-generic/int-ll64.h>
+
+typedef struct mutex {
+    int lock;       /* Estado do cadeado: 0 para livre, 1 para ocupado */
+    char *name;     /* Nome do mutex para fins de depuração */
+    u64    lock_byte;
+#ifdef ARM32 
+   u64    lock_arm64;
+#else I386
+   u64    lock_i64;
+#endif
+} *mutex_t;
+
+#define MUTEX_INITIALIZER { 0, 0 }
+
